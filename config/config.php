004ff966dc3b6aa57afd51be41bde08974d81e58
Adrián Jaramillo Rodríguez <adristudy@gmail.com>
2021-11-14 13:04:53 +0000

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocr2lp6lisyt',
+  'passwordsalt' => 'RjrrSdcDyvgGXhx/Grnoccv298gljL',
+  'secret' => 'b7JkIA6v1+MGT70voUpA9XOvvMRVAc/PsgImxoDmYQytgHut',
+  'trusted_domains' => 
+  array (
+    0 => 'www.nextcloud-adrianj.com',
+  ),
+  'datadirectory' => '/var/www/nextcloud/cloud/data',
+  'dbtype' => 'mysql',
+  'version' => '22.2.1.2',
+  'overwrite.cli.url' => 'http://www.nextcloud-adrianj.com/cloud',
+  'dbname' => 'nextcloud_db',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud_user',
+  'dbpassword' => '1234',
+  'installed' => true,
+);
---
+++ version.php
@@ -0,0 +1,20 @@
+<?php 
+$OC_Version = array(22,2,1,2);
+$OC_VersionString = '22.2.1';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '21.0' => true,
+    '22.0' => true,
+    '22.1' => true,
+    '22.2' => true,
+  ),
+  'owncloud' => 
+  array (
+    '10.5' => true,
+  ),
+);
+$OC_Build = '2021-11-11T14:48:06+00:00 c8c18d8d40225fb441d05267cb11a64792953095';
+$vendor = 'nextcloud';
